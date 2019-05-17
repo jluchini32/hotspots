85656b30b699b790c9921a28aa76daf0a13a64e0
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-05-17 12:08:37 -0600

    styling

--- hotspots-app/src/MapContainer/MapContainer.js
+++ hotspots-app/src/MapContainer/MapContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import HotspringContainer from '../HotspringContainer/HotspringContainer';
 import CreateHotspring from '../CreateHotspring/CreateHotspring';
-import icon from '../firedept.png';
+import icon from '../fire.png';
 
 const Zebra = ({ text }) => <div>{text}<img src={icon}></img></div>;
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
@@ -14,7 +14,8 @@ class MapContainer extends Component {
       lat: 39.336483976183864,
       lng: -104.88013671875001
     },
-    zoom: 6.5
+    zoom: 6.5,
+    scaleControl: true
 
 
   };
@@ -28,8 +29,8 @@ class MapContainer extends Component {
             lat={spring.lat}
             lng={spring.lng}
             text={spring.name}
-            icon={"/firedept.png"}
-         
+            icon={"/fire.png"}
+            
           
           />
       )
